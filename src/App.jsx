b1d4b553413f7ac132c27f79c6701d816533010b
Fernando Cancino <fcancino92@gmail.com>
2022-08-09 23:45:43 -0300

    entrega 6 problema solucionado

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/NavBar/NavBar';
 import Contador from './components/Contador/Contador';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
-import ItemList from './components/ItemList/ItemList';
 import ItemDetailConteiner from './components/ItemDetailConteiner/ItemDetailConteiner';
 
 
@@ -13,8 +12,8 @@ function App() {
     return (
     <>
       <NavBar />
-      <ItemListContainer/>
       <Contador/>
+      <ItemListContainer/>
       <ItemDetailConteiner/>
     </>
   )
